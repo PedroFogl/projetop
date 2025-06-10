2c33b1470ca5d24d4f3400c04bfbd121a31d79c0
Pedro Foglieni <pedro.pimentel@preve.g12.br>
2025-06-10 16:55:36 -0300

    Criaçao do busca cep

---
+++ app/_layout.tsx
@@ -0,0 +1,26 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { LogBox } from "react-native";
+
+LogBox.ignoreAllLogs(true);
+
+export default function RootLayout() {
+  return (
+  <>
+  <StatusBar style="light" />
+  
+  <Stack
+  screenOptions = {{
+        headerStyle:{ backgroundColor:'#25292e',},
+        headerShadowVisible: false,
+        headerTintColor: 'black',
+    }}
+    >
+        
+    <Stack.Screen name = "(tabs)" options ={{ headerShown: false }} />
+    <Stack.Screen name = "+not-found"/>
+    
+    </Stack>;
+  </>
+  )
+}
